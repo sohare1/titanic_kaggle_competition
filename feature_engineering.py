f96d6bd32ad91dce14d69417b9dc08b0c8342e27
sohare <78981539+sohare1@users.noreply.github.com>
2025-04-15 21:02:46 -0700

    adding title map

--- feature_engineering.py
+++ feature_engineering.py
@@ -2,20 +2,40 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class FeatureEngineer(BaseEstimator, TransformerMixin):
-    """Custom transformer to extract features like Title, FamilySize, and IsAlone from Titanic data."""
+    """Extracts Title, FamilySize, IsAlone from Titanic dataset."""
+
+    def __init__(self):
+        self.title_map = {
+            "Mr": "Mr",
+            "Mrs": "Mrs",
+            "Miss": "Miss",
+            "Ms": "Miss",
+            "Mlle": "Miss",
+            "Mme": "Mrs",
+            "Master": "Master",
+            "Dr": "Officer",
+            "Rev": "Officer",
+            "Col": "Officer",
+            "Major": "Officer",
+            "Capt": "Officer",
+            "Sir": "Royalty",
+            "Lady": "Royalty",
+            "Don": "Royalty",
+            "Jonkheer": "Royalty",
+            "Countess": "Royalty"
+        }
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
         X_ = X.copy()
-        X_['Title'] = X_['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
-        X_['Title'] = X_['Title'].replace([
-            'Lady', 'Countess', 'Capt', 'Col', 'Don', 'Dr',
-            'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'
-        ], 'Rare')
-        X_['Title'] = X_['Title'].replace({'Mlle': 'Miss', 'Ms': 'Miss', 'Mme': 'Mrs'})
 
+        # Title Extraction & Mapping
+        X_['Title'] = X_['Name'].str.extract(' ([A-Za-z]+)\.', expand=False)
+        X_['Title'] = X_['Title'].map(self.title_map).fillna('Other')
+
+        # Family Size Features
         X_['FamilySize'] = X_['SibSp'] + X_['Parch'] + 1
         X_['IsAlone'] = (X_['FamilySize'] == 1).astype(int)
 
